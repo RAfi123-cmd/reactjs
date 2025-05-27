58a6015c4396e6b5f8ef283c89af6cfb2c3d7d19
Rafi123-CMD <rafiulhuda27@gmail.com>
2025-05-28 01:10:53 +0700

    contoh JSX

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // const userLogged = "Rafi BWA";
+  // const userLoggedJSX = <strong>BWA for Future!</strong>;
+
+  let a=2;
+  let b=3;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {a+b / 2}
     </div>
   );
 }
