525265a960e83355cb27b34bbfe965ba20bbbcf9
Rafi123-CMD <rafiulhuda27@gmail.com>
2025-05-28 15:40:40 +0700

    Design web

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 
 function App() {
 
+  const category = "RUNNING";
+  const price = 74000000;
+
   return (
-    <div>
-      <h1>Belajar with Rafi</h1>
-      <p>Meningkatkan keahlihan design dan coding secara online dan cepat.</p>
-      <h1>Belajar with Rafi</h1>
-      <p>Meningkatkan keahlihan design dan coding secara online dan cepat.</p>
-      <h1>Belajar with Rafi</h1>
-      <p>Meningkatkan keahlihan design dan coding secara online dan cepat.</p>
+    <div class="Parentbox">
+      <div class="Foto">
+        <img src="sneaker.jpg" />
+      </div>
+      <div class="Deskripsi">
+        <p class="Cate">{category}</p>
+        <h1 class="Title">Pinky Swag</h1>
+        <p class="Price">IDR {price}</p>
+        <p class="Info">
+          One the most recognizable shoes in the AJ collection, the Air Jordan 3 Retro features
+          lightweight, visible cushioning just like the original from '88. Signature details and
+          meterials celebrate the game-changing icon</p>
+      </div>
     </div>
   );
 }
