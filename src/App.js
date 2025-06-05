b8200fbe27cd43f19018627cccd1484e5919aa39
Rafi123-CMD <rafiulhuda27@gmail.com>
2025-06-05 15:27:07 +0700

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="Parentbox">
       <FotoProduk />
-      <ProdukInfo name="Sneaker" category="LEBARAN" />
+      <ProdukInfo isDiscount="yes" name="Sneaker" category="LEBARAN" />
     </div>
   );
 }
@@ -19,14 +19,32 @@ function FotoProduk(){
   );
 }
 
+function CheckDiscount(props){
+  const { isDiscount } = props;
+  if (isDiscount == "yes") {
+    return (
+      <p>Diskon 50% Off</p>
+    );
+  }else if (isDiscount == "coming") {
+    return (
+      <p>akan ada Diskon ...</p>
+    );
+  }else{
+    return (
+      <p>Belum ada diskon</p>
+    );
+  }
+}
+
 function ProdukInfo(props){
-  const { category, name } = props;
+  const { category, name, isDiscount } = props;
   return (
     <div>
       <div className="Deskripsi">
         <p className="Cate">{category}</p>
         <h1 className="Title">{name}</h1>
         <p className="Price">IDR 73.309.399</p>
+        <CheckDiscount isDiscount={isDiscount} />
         <p className="Info">
           One the most recognizable shoes in the AJ collection, the Air Jordan 3 Retro features
           lightweight, visible cushioning just like the original from '88. Signature details and
